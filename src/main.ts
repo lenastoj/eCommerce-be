17af5427f8e291159f675643d2a4132b349389dc
KostaDokosic <kosta99dt@gmail.com>
2023-06-07 13:06:56 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import createApp from "@app/app";
-import "@services/database.service";
+import createApp from '@app/app';
+import '@services/database.service';
+import path from 'path';
 
 const app = createApp();
 
@@ -8,6 +9,5 @@ const port = process.env.PORT ? Number(process.env.PORT) : 4000;
 
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
-  console.log('dirname', __dirname)
-
+  console.log('dirname', path.join(process.cwd(), 'public'));
 });
