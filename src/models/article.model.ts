e5d87ddb9e5fa1a855581e768fc49ba393cc6e1d
KostaDokosic <kosta99dt@gmail.com>
2023-06-07 13:58:33 +0200

    db fixes

--- src/models/article.model.ts
+++ src/models/article.model.ts
@@ -4,56 +4,52 @@ import {
   Column,
   DataType,
   BelongsToMany,
+  ForeignKey,
+  BelongsTo,
+  AllowNull,
+  NotEmpty,
 } from 'sequelize-typescript';
 import Color from './color.model';
 import Size from './size.model';
 import { ArticleColor } from './articleColor.model';
 import { ArticleSize } from './articleSize.model';
+import User from './user.model';
 
 @Table({
   timestamps: true,
   tableName: 'articles',
 })
 export default class Article extends Model {
-  @Column({
-    type: DataType.STRING,
-    allowNull: false,
-  })
-  public declare name: string;
+  @Column(DataType.STRING)
+  name: string;
 
-  @Column({
-    type: DataType.TEXT,
-    allowNull: false,
-  })
-  public declare description: string;
+  @Column(DataType.TEXT)
+  description: string;
 
-  @Column({
-    type: DataType.STRING,
-    allowNull: false,
-  })
-  public declare imageUrl: string;
+  @Column(DataType.STRING)
+  imageUrl: string;
 
-  @Column({
-    type: DataType.FLOAT,
-    allowNull: false,
-  })
-  public declare price: number;
+  @Column(DataType.DOUBLE)
+  price: number;
 
-  @Column({
-    type: DataType.BOOLEAN,
-    allowNull: false,
-  })
-  public declare inStock: boolean;
+  @Column(DataType.BOOLEAN)
+  inStock: boolean;
 
-  @Column({
-    type: DataType.ENUM('man', 'woman'),
-    allowNull: false,
-  })
-  public declare gender: 'man' | 'woman';
+  @Column(DataType.ENUM('man', 'woman'))
+  gender: 'man' | 'woman';
 
   @BelongsToMany(() => Color, () => ArticleColor)
-  public declare colors: Color[];
+  colors: Color[];
 
   @BelongsToMany(() => Size, () => ArticleSize)
-  public declare sizes: Size[];
+  sizes: Size[];
+
+  @AllowNull(false)
+  @NotEmpty
+  @ForeignKey(() => User)
+  @Column(DataType.INTEGER)
+  userId: number;
+
+  @BelongsTo(() => User)
+  user: ReturnType<() => User>;
 }
